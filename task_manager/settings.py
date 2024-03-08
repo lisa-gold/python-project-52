e5238db6e28a9a81abf077c269872c09bd470e40
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-03-08 17:11:42 +0200

    some translation

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 from dotenv import load_dotenv
+from django.utils.translation import gettext_lazy as _
 
 load_dotenv()
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,13 +132,22 @@ ROLLBAR = {
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = [
+    ('ru', _('Russian')),
+    ('en', _('English')),
+]
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LOCALE_PATHS = ( os.path.join(BASE_DIR, 'locale'), )
 
 
 # Static files (CSS, JavaScript, Images)
--- task_manager/views.py
+++ task_manager/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 
 
 MESSAGE_TAGS = {
@@ -24,10 +25,10 @@ class Login(SuccessMessageMixin, LoginView):
     form_class = AuthenticationForm
     template_name = 'login.html'
     success_url = reverse_lazy('index')
-    success_message = 'Successfully logged in!'
+    success_message = _('Successfully logged in!')
 
 
 def logout_view(request):
     logout(request)
-    messages.add_message(request, messages.INFO, 'Successfully logged out!')
+    messages.add_message(request, messages.INFO, _('Successfully logged out!'))
     return redirect('index')
