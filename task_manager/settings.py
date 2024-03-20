6a27c7f4b29652c287ee80f197042f236b57ad55
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-03-20 10:52:19 +0200

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'django_bootstrap5',
     'task_manager',
     'task_manager.users',
